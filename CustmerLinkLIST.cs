60d107c1c78f238d058e07baa7fc67ad6bd9dd64
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-09-14 13:09:01 +0530

    [roshan]uc2-to create Linked

--- CustmerLinkLIST.cs
+++ CustmerLinkLIST.cs
@@ -3,45 +3,120 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace linkLIASt2
 {
     internal class CustmerLinkLIST
     {
-        Nodes head;
-        public void AddEnd(int dataNew)
-        {
-            Nodes NewData=new Nodes(dataNew);
 
-            if(this.head == null)
+        internal Nodes head;
+
+        internal void Add(int data)
+        {
+            Nodes node = new Nodes(data);
+            if (this.head == null)
             {
-                this.head=NewData; 
+                this.head = node;
             }
             else
             {
-                Nodes temp=head;
+                Nodes temp = head;
                 while (temp.next != null)
                 {
-                    temp=temp.next;
-
+                    temp = temp.next;
                 }
-                temp.next=NewData;
-              
+                temp.next = node;
             }
+            Console.WriteLine("{0} inserted into linked list", node.data);
         }
-        public void Display()
+        internal void Display()
         {
-            Nodes temp=this. head;
-            if(temp==null)
+            Nodes temp = this.head;
+            if (temp == null)
             {
-                Console.WriteLine("link list is empty");
+                Console.WriteLine("LinkedList is empty");
+                return;
             }
-            while(temp!=null)
+            while (temp != null)
             {
-                Console.WriteLine(temp.data);
-                temp=temp.next;
+                Console.Write(temp.data + " ");
+                temp = temp.next;
             }
+            Console.WriteLine();
         }
-       
+
+
+        public bool InsertAfter(int data, int after)    //insert in between the sequences
+        {
+            bool flag = false;
+            if (head == null)
+                Console.WriteLine("List is Empty");
+            else
+            {
+                Nodes temp = head;
+                while (temp != null)
+                {
+                    if (temp.data == after)
+                    {
+                        Nodes newNode = new Nodes(data);
+                        newNode.next = temp.next;
+                        temp.next = newNode;
+                        flag = true;
+                        break;
+                    }
+                    temp = temp.next;
+                }
+                if (!flag)
+                    Console.WriteLine("The Element is Absent");
+            }
+            return flag;
+        }
+
+        internal int Search(int value)
+        {
+            Nodes node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
+        internal Nodes InsertNthPosition(int position, int data)
+        {
+            if (position < 1)
+                Console.WriteLine("Invalid Position");
+            if (position == 1)
+            {
+                var newNode = new Nodes(data);
+                newNode.next = this.head;
+                head = newNode;
+            }
+            else
+            {
+                while (position-- != 0)
+                {
+                    if (position == 1)
+                    {
+                        Nodes newNode = new Nodes(data);
+                        newNode.next = this.head.next;
+                        head.next = newNode;
+                        break;
+                    }
+                    head = head.next;
+                }
+                if (position != 1)
+                    Console.WriteLine("Position out of range");
+            }
+            return head;
+        }
+
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,17 +5,28 @@
         static void Main(string[] args)
         {
 
-            Console.WriteLine("chooes Number\n1.simple Linked List");
+            Console.WriteLine("chooes Number\n1.simple Linked List\n2.to create Linked");
             int opt=Convert.ToInt32(Console.ReadLine());
             switch(opt)
             {
                 case 1:
                     CustmerLinkLIST custmerLinkLIST = new CustmerLinkLIST();
-                    custmerLinkLIST.AddEnd(56);
-                    custmerLinkLIST.AddEnd(30);
-                    custmerLinkLIST.AddEnd(70);
+                    custmerLinkLIST.Add(56);
+                    custmerLinkLIST.Add(30);
+                    custmerLinkLIST.Add(70);
 
                     custmerLinkLIST.Display();
+                    break;
+
+                    case 2:
+                    CustmerLinkLIST list2 = new CustmerLinkLIST();
+                    list2.Add(70);
+                    int a = list2.Search(70);
+                    Console.WriteLine("index position of 70 is : " + a);
+                    list2.InsertNthPosition(a + 1, 30);
+                    list2.InsertNthPosition(a + 1, 56);
+                    list2.Display();
+                    Console.WriteLine("****Program End***\n");
                     break;
             }
 
